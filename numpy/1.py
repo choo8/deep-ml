11898ee48b8d4407cfd6c6b407b49a1e63d2ba20
choo8 <joshua.choo.1993@gmail.com>
2025-11-13 08:16:42 +0800

    Changed to numpy implementation

--- numpy/1.py
+++ numpy/1.py
@@ -1,18 +1,19 @@
+import numpy as np
+
+
 # Matrix-Vector Dot Product
 
 # Write a Python function that computes the dot product of a matrix and a vector. The function should return a list representing the resulting vector if the operation is valid, or -1 if the matrix and vector dimensions are incompatible. A matrix (a list of lists) can be dotted with a vector (a list) only if the number of columns in the matrix equals the length of the vector. For example, an n x m matrix requires a vector of length m.
 
 def matrix_dot_vector(a: list[list[int|float]], b: list[int|float]) -> list[int|float]:
-    m, n = len(a), len(a[0])
-    k = len(b)
+    try:
+        mat = np.array(a)
+        v = np.array(b)
 
-    if n != k:
+        return (mat @ v).tolist()
+    except ValueError:
         return -1
-    
-    out = []
-    for row in a:
-        out.append(sum([i * j for i, j in zip(row, b)]))
-    return out
+
 
 if __name__ == "__main__":
     a = [[1, 2], [2, 4]]
